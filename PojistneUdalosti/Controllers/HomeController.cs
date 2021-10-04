2c51bb8918e540f1c2623ed3e6b0a9d77553b1c1
Kateřina Mlázovská <60098902+Virtualll001@users.noreply.github.com>
2021-10-04 14:27:14 +0200

    2 - změna uspořádání projektu
    3 knihovny + úprava referencí

--- PojistneUdalosti/Controllers/HomeController.cs
+++ PojistneUdalosti/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PojistneUdalosti.Models;
+using PojistneUdalosti.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
