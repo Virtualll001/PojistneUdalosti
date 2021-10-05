d58263965fd6902124183323cc09686f09701bbe
Kateřina Mlázovská <60098902+Virtualll001@users.noreply.github.com>
2021-10-05 10:43:41 +0200

    5 - implementace Repository a Stored Procedures

---
+++ PojistneUdalosti.DataAccess/Repository/IRepository/ISP_Call.cs
@@ -0,0 +1,20 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PojistneUdalosti.DataAccess.Repository.IRepository
+{
+    public interface ISP_Call : IDisposable //"Stored Procedures"
+    {
+        T Single<T>(string procedureName, DynamicParameters param = null); //DynamicParameters => NuGet Dapper
+
+        void Execute(string procedureName, DynamicParameters param = null);
+        T OneRecord<T>(string procedureName, DynamicParameters param = null);
+        IEnumerable<T> List<T>(string procedureName, DynamicParameters param = null);
+        Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedureName, DynamicParameters param = null);
+        //Tuple = návratový typ pro více tabulek
+    }
+}
---
+++ PojistneUdalosti.DataAccess/Repository/PojisteniRepository.cs
@@ -0,0 +1,34 @@
+﻿using PojistneUdalosti.DataAccess.Data;
+using PojistneUdalosti.DataAccess.Repository.IRepository;
+using PojistneUdalosti.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PojistneUdalosti.DataAccess.Repository
+{
+    //tato třída řeší update - ostatní metody jsou v Repository
+    public class PojisteniRepository : Repository<Pojisteni>, IPojisteniRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public PojisteniRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Pojisteni pojisteni)
+        {
+            var objFromDb = _db.Pojisteni.FirstOrDefault(s => s.PojisteniId == pojisteni.PojisteniId);
+            
+            if(objFromDb!=null)
+            {
+                objFromDb.TypPojisteni = pojisteni.TypPojisteni;
+                objFromDb.Podminky = pojisteni.Podminky;
+                objFromDb.Zaloha = pojisteni.Zaloha;
+                _db.SaveChanges();
+            }            
+        }
+    }
+}
